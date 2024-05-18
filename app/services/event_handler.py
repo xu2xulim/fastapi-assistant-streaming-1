7d2fe554f12b435de42409a23c207a8de0033109
Sunny Lim <limhss@gmail.com>
2024-05-19 07:03:47 +0800

    A

--- app/services/event_handler.py
+++ app/services/event_handler.py
@@ -31,7 +31,38 @@ class EventHandler(AsyncAssistantEventHandler):
     async def on_end(self) -> None:
         """Fires when stream ends or when exception is thrown"""
         self.done.set()
-
+    
+    @override
+    async def on_event(self, event):
+      # Retrieve events that are denoted with 'requires_action'
+      # since these will have our tool_calls
+      if event.event == 'thread.run.requires_action':
+        run_id = event.data.id  # Retrieve the run ID from the event data
+        self.handle_requires_action(event.data, run_id)
+ 
+    async def handle_requires_action(self, data, run_id):
+      tool_outputs = []
+        
+      for tool in data.required_action.submit_tool_outputs.tool_calls:
+        if tool.function.name == "get_random_digit":
+          tool_outputs.append({"tool_call_id": tool.id, "output": "57"})
+        elif tool.function.name == "get_random_letter":
+          tool_outputs.append({"tool_call_id": tool.id, "output": "X"})
+        
+      # Submit all tool_outputs at the same time
+      self.submit_tool_outputs(tool_outputs, run_id)
+ 
+    async def submit_tool_outputs(self, tool_outputs, run_id):
+      # Use the submit_tool_outputs_stream helper
+      with self.client.beta.threads.runs.submit_tool_outputs_stream(
+        thread_id=self.current_run.thread_id,
+        run_id=self.current_run.id,
+        tool_outputs=tool_outputs,
+        event_handler=EventHandler(),
+      ) as stream:
+        for text in stream.text_deltas:
+          print(text, end="", flush=True)
+        print()
 
     def on_tool_call_created(self, tool_call):
         print(f"\nassistant > {tool_call.type}\n", flush=True)
