95f3808b1d9fb9ac138f5b13d011b9898206a416
Sunny Lim <limhss@gmail.com>
2024-05-18 16:03:06 +0800

    A

--- app/services/assistant_service.py
+++ app/services/assistant_service.py
@@ -3,43 +3,9 @@ import asyncio
 from openai import AsyncOpenAI #, AssistantEventHandler
 
 from app.core.config import settings
-from app.services.event_handler import EventHandler
+from app.services.event_handler2 import EventHandler
 
-"""
-class EventHandler(AssistantEventHandler):
-
-    #@override
-    async def on_event(self, event):
-      # Retrieve events that are denoted with 'requires_action'
-      # since these will have our tool_calls
-      if event.event == 'thread.run.requires_action':
-        run_id = event.data.id  # Retrieve the run ID from the event data
-        self.handle_requires_action(event.data, run_id)
-    
-    async def handle_requires_action(self, data, run_id):
-      tool_outputs = []
-        
-      for tool in data.required_action.submit_tool_outputs.tool_calls:
-        if tool.function.name == "get_random_digit":
-          tool_outputs.append({"tool_call_id": tool.id, "output": "7"})
-        elif tool.function.name == "get_random_letter":
-          tool_outputs.append({"tool_call_id": tool.id, "output": "X"})
-        
-      # Submit all tool_outputs at the same time
-      self.submit_tool_outputs(tool_outputs, run_id)
  
-    async def submit_tool_outputs(self, tool_outputs, run_id):
-      # Use the submit_tool_outputs_stream helper
-      with self.client.beta.threads.runs.submit_tool_outputs_stream(
-        thread_id=self.current_run.thread_id,
-        run_id=self.current_run.id,
-        tool_outputs=tool_outputs,
-        event_handler=EventHandler(),
-      ) as stream:
-        for text in stream.text_deltas:
-          print(text, end="", flush=True)
-        print()
-"""
 class AssistantService:
     client: AsyncOpenAI
     assistant_id: str
