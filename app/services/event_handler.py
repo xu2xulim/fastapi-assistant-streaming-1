42fafa509c942cc5d2d43de386c04e5bdd9833f6
Sunny Lim <limhss@gmail.com>
2024-05-19 11:31:42 +0800

    A

--- app/services/event_handler.py
+++ app/services/event_handler.py
@@ -38,17 +38,17 @@ class EventHandler(AsyncAssistantEventHandler):
     
     @override
     async def on_event(self, event):
-        detalog.put({"log" : "on_event", "check" : str(event)}, expire_in=120)
+        #detalog.put({"log" : "on_event", "check" : str(event)}, expire_in=120)
         # Retrieve events that are denoted with 'requires_action'
         # since these will have our tool_calls
         if event.event == 'thread.run.requires_action':
-            detalog.put({"log" : "thread.run.requires_action", "check" : event.data.id}, expire_in=120)
+            detalog.put({"log" : "thread.run.requires_action", "check" : str(event)}, expire_in=120)
             run_id = event.data.id  # Retrieve the run ID from the event data
             #handle_requires_action(event.data, run_id)
 
 
     #def handle_requires_action(self, data, run_id):
-            detalog.put({"log" : "handle_requires_action", "check" : "I am here"}, expire_in=120)
+            #detalog.put({"log" : "handle_requires_action", "check" : "I am here"}, expire_in=120)
             tool_outputs = []
         
             for tool in event.data.required_action.submit_tool_outputs.tool_calls:
@@ -61,7 +61,7 @@ class EventHandler(AsyncAssistantEventHandler):
     #submit_tool_outputs(tool_outputs, run_id)
 
     #def submit_tool_outputs(self, tool_outputs, run_id):    
-            detalog.put({"log" : "submit_tool_outputs", "check" : tool_outputs}, expire_in=120)
+            #detalog.put({"log" : "submit_tool_outputs", "check" : tool_outputs}, expire_in=120)
     # Use the submit_tool_outputs_stream helper
             with self.client.beta.threads.runs.submit_tool_outputs_stream(
                 #thread_id=run_id,
@@ -71,13 +71,14 @@ class EventHandler(AsyncAssistantEventHandler):
             ) as stream:
                 for text in stream.text_deltas:
                     print(text, end="", flush=True)
-                detalog.put({"log" : "End of submit_tool_outputs", "check" : tool_outputs}, expire_in=120)
+            
+            detalog.put({"log" : "End of submit_tool_outputs", "check" : tool_outputs}, expire_in=120)
  
     async def on_tool_call_created(self, tool_call):
         detalog.put({"log" : "on_tool_call_created", "check" : "OK"}, expire_in=120)
   
     async def on_tool_call_delta(self, delta, snapshot):
-        detalog.put({"log" : "on_tool_call_delta", "check" : delta.type}, expire_in=120)
+        #detalog.put({"log" : "on_tool_call_delta", "check" : delta.type}, expire_in=120)
         if delta.type == 'code_interpreter':
             if delta.code_interpreter.input:
                 print(delta.code_interpreter.input, end="", flush=True)
@@ -89,8 +90,8 @@ class EventHandler(AsyncAssistantEventHandler):
                     else:
                         print(f"\nOutput Type: {output.type}", flush=True)
         elif delta.type == 'function':
-            detalog.put({"log" : "on_tool_call_delta - input", "check" : delta.function.arguments}, expire_in=120)
-            detalog.put({"log" : "on_tool_call_delta - output", "check" : delta.function.output}, expire_in=120)
+            #detalog.put({"log" : "on_tool_call_delta - input", "check" : delta.function.arguments}, expire_in=120)
+            #.put({"log" : "on_tool_call_delta - output", "check" : delta.function.output}, expire_in=120)
             print(f"Delta Type: {delta.type}", flush=True)
         else:
             print(f"Delta Type: {delta.type} not supported", flush=True)
