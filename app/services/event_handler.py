bc81ae9209c992c226226104b87dd121e4040fe2
Sunny Lim <limhss@gmail.com>
2024-05-19 08:56:12 +0800

    A

--- app/services/event_handler.py
+++ app/services/event_handler.py
@@ -3,6 +3,10 @@ from typing import AsyncIterator, Literal, Union, cast
 
 from openai import AsyncAssistantEventHandler
 from typing_extensions import override
+import os
+from deta import Deta
+DETA_DATA_KEY = os.environ,get('DETA_DATA_KEY')
+detalog = Deta(DETA_DATA_KEY).Base('assistant')
 
 
 class EventHandler(AsyncAssistantEventHandler):
@@ -42,36 +46,36 @@ class EventHandler(AsyncAssistantEventHandler):
         self.handle_requires_action(event.data, run_id)
  
     def handle_requires_action(self, data, run_id):
-      print(f"\nhandle_requires_action > {data}\n", flush=True)
-      tool_outputs = []
+        detalog.put({"log" : "handle_requires_action", "check" : data}, expire_in=120)
+        tool_outputs = []
         
-      for tool in data.required_action.submit_tool_outputs.tool_calls:
-        if tool.function.name == "get_random_digit":
-          tool_outputs.append({"tool_call_id": tool.id, "output": "57"})
-        elif tool.function.name == "get_random_letter":
-          tool_outputs.append({"tool_call_id": tool.id, "output": "X"})
+        for tool in data.required_action.submit_tool_outputs.tool_calls:
+            if tool.function.name == "get_random_digit":
+                tool_outputs.append({"tool_call_id": tool.id, "output": "57"})
+            elif tool.function.name == "get_random_letter":
+                tool_outputs.append({"tool_call_id": tool.id, "output": "X"})
         
-      # Submit all tool_outputs at the same time
-      self.submit_tool_outputs(tool_outputs, run_id)
+        # Submit all tool_outputs at the same time
+        self.submit_tool_outputs(tool_outputs, run_id)
  
     def submit_tool_outputs(self, tool_outputs, x_run_id):
-      print(f"\nsubmit_tool_outputs > {tool_outputs}\n", flush=True)
-      # Use the submit_tool_outputs_stream helper
-      with self.client.beta.threads.runs.submit_tool_outputs_stream(
-        thread_id=x_run_id,
-        run_id=x_run_id,
-        tool_outputs=tool_outputs,
-        event_handler=EventHandler(),
-      ) as stream:
-        for text in stream.text_deltas:
-          print(text, end="", flush=True)
+        detalog.put({"log" : "submit_tool_outputs", "check" : tool_outputs}, expire_in=120)
+        # Use the submit_tool_outputs_stream helper
+        with self.client.beta.threads.runs.submit_tool_outputs_stream(
+            thread_id=x_run_id,
+            run_id=x_run_id,
+            tool_outputs=tool_outputs,
+            event_handler=EventHandler(),
+        ) as stream:
+            for text in stream.text_deltas:
+                print(text, end="", flush=True)
         print("I am here")
 
     async def on_tool_call_created(self, tool_call):
-        print(f"\non_tool_call_created > {tool_call.type}\n", flush=True)
+        detalog.put({"log" : "on_tool_call_created", "check" : "OK"}, expire_in=120)
   
     async def on_tool_call_delta(self, delta, snapshot):
-        print(f"\non_tool_call_delta > {delta}\n", flush=True)
+        detalog.put({"log" : "on_tool_call_delta", "check" : "OK"}, expire_in=120)
         if delta.type == 'code_interpreter':
             if delta.code_interpreter.input:
                 print(delta.code_interpreter.input, end="", flush=True)
