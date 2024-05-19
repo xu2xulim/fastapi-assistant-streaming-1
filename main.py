200d2493b7d79bf59ec9857156c213dd5c5ae38b
Sunny Lim <limhss@gmail.com>
2024-05-19 16:16:13 +0800

    A

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import logging
 
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 from app.api.v1 import assistant
 from app.core.config import settings
@@ -11,7 +12,13 @@ app = FastAPI(
     title=settings.PROJECT_NAME,
     version=settings.PROJECT_VERSION,
 )
-
+app.add_middleware(
+  CORSMiddleware,
+  allow_origins=['*'],
+  allow_credentials=True,
+  allow_methods=['*'],
+  allow_headers=['*'],
+)
 app.include_router(assistant.router, prefix=settings.API_V1_STR)
 
 
