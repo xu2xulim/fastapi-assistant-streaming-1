a6d6caa37c49ffe4aa808c9588de4fd46420d164
Sunny Lim <limhss@gmail.com>
2024-05-23 13:01:08 +0800

    A

--- app/services/event_handler.py
+++ app/services/event_handler.py
@@ -69,9 +69,10 @@ class EventHandler(AsyncAssistantEventHandler):
                     "OpenAI-Beta" : "assistants=v2",
                     "Authorization" : f"Bearer {OPENAI_API_KEY}"}
                 
-                res = requests.post(f"https://api.openai.com/v1/threads/{event.data.thread_id}/runs/{event.data.id}/submit_tool_outputs", json={"tool_outputs" : tool_outputs}, headers=headers)
+                res = requests.post(f"https://api.openai.com/v1/threads/{event.data.thread_id}/runs/{event.data.id}/submit_tool_outputs", json={"tool_outputs" : tool_outputs, "stream" : True}, headers=headers)
                 try:
-                    self.queue.put_nowait(f"I am faking this output {res.blob()}")
+                    self.queue.put_nowait(f"I am faking this output\n")
+                    self.queue.put_nowait(f"I am faking this output {res.blob().stream}")
                 except:
                     pass
                 detalog.put({"log" : "submit_tool_outputs", "check" : str(res.text)}, expire_in=120)
